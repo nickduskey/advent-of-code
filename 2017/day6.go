968c7be398e79f08d1588883921589d37d36a8cc
Nick Duskey <nduskey@smartlink.city>
2017-12-06 15:55:20 -0500

    Solved d6p2

--- 2017/day6.go
+++ 2017/day6.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-func day6p1() int {
+func day6p1() (int, [16]int) {
 	banks := [16]int{14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4}
-	// fmt.Println("starting banks:", banks)
 	var cycles, largest, largestIndex int
 	var states [][16]int
 	states = append(states, banks)
-	// fmt.Println("starting states:", states)
 	for {
+		// Increment the cycle count
+		cycles++
+
 		largest = 0
 		largestIndex = 0
 		// Get the largest bank
@@ -19,49 +20,73 @@ func day6p1() int {
 				largestIndex = i
 			}
 		}
-		// Distribute the largest bank evenly
-		q := largest / len(banks)
-		r := largest % len(banks)
 
-		// Handle q that is less than len(banks)
-		if q == 0 {
-			banks[largestIndex] = 0
-			for i := 0; r > 0; i++ {
-				if i == largestIndex {
-					continue
-				}
-				banks[i]++
-				r--
-			}
-		}
-		// if even
-		if r == 0 {
-			banks[largestIndex] = 0
-			// add q to all positions of banks
-			for i := 0; i < len(banks); i++ {
-				banks[i] += q
-			}
-		}
-		// if remainder
-		if r != 0 {
-			fmt.Println("q:", q)
-			fmt.Println("r:", r)
-			// first set largestIndex to 0
-			banks[largestIndex] = 0
+		val := largest
 
-			// give uneven to the largestIndex
-			for i := 0; i < len(banks); i++ {
-				if i == largestIndex {
-					banks[i] += r
-				} else {
-					banks[i] += q
-				}
+		banks[largestIndex] = 0
+		i := largestIndex + 1
+		if i > (len(banks) - 1) {
+			i = 0
+		}
+
+		for val > 0 {
+			banks[i]++
+			i++
+			if i > (len(banks) - 1) {
+				i = 0
 			}
-			// fmt.Println("banks after modification:", banks)
+			val--
 		}
 
 		// Check the modified banks against the previously seen states
-		for i, state := range states {
+		for _, state := range states {
+			if banks == state {
+				return cycles, banks
+			}
+		}
+
+		// If unseen save the current banks as a new state in states
+		states = append(states, banks)
+	}
+}
+
+func day6p2(banks [16]int) int {
+	var cycles, largest, largestIndex int
+	var states [][16]int
+	states = append(states, banks)
+	for {
+		// Increment the cycle count
+		cycles++
+
+		largest = 0
+		largestIndex = 0
+		// Get the largest bank
+		for i, bank := range banks {
+			if bank > largest {
+				largest = bank
+				largestIndex = i
+			}
+		}
+
+		val := largest
+
+		banks[largestIndex] = 0
+		i := largestIndex + 1
+		if i > (len(banks) - 1) {
+			i = 0
+		}
+
+		for val > 0 {
+			banks[i]++
+			i++
+			if i > (len(banks) - 1) {
+				i = 0
+			}
+			val--
+		}
+
+		// Check the modified banks against the previously seen states
+		for _, state := range states {
 			if banks == state {
 				return cycles
 			}
@@ -69,13 +94,12 @@ func day6p1() int {
 
 		// If unseen save the current banks as a new state in states
 		states = append(states, banks)
-
-		// Increment the cycle count
-		cycles++
 	}
 }
 
 func main() {
-	cycles := day6p1()
+	cycles, banks := day6p1()
+	addlcycles := day6p2(banks)
 	fmt.Println("cycles:", cycles)
+	fmt.Println("addlcycles:", addlcycles)
 }
